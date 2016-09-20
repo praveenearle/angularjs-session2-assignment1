30153e6b33f233707ed6279ae541fce632c4f71e
praveenearle <praveen.earle@gmail.com>
2016-09-20 10:23:49 +0530

    angular js s2a1
    angularjs s2a1

---
+++ angular js s2a1/angular js s1a2/scripts/script.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.name = "james bond";
+    $scope.employeeid = "007";
+    $scope.designation = "team lead";
+
+
+        
+    }); 
+
